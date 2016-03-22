39bb9a4433c98cbe577cb27c466e62f76c921a5f
SirSavary <sirsavary@outlook.com>
2016-03-22 01:16:47 -0400

    Cleanup MarkBookReader.java

--- src/main/java/sample/MarkBookReader.java
+++ src/main/java/sample/MarkBookReader.java
@@ -3,13 +3,9 @@ package sample;
 import java.io.*;
 import java.util.LinkedHashMap;
 
-/**
- * Created by Max on 3/6/2016.
- */
 public class MarkBookReader {
     public static String currentUser;
-    public static String invalidfFirstName = "User";
-    public static String invalidLastName = "Invalid";
+
     public static LinkedHashMap<String, String> readMarkBook() throws IOException {
         currentUser = getCurrentUser();
         InputStream stream = MarkBookReader.class.getResourceAsStream("/markbook/markbook.txt");
@@ -20,22 +16,19 @@ public class MarkBookReader {
         String currentLine = bufferedReader.readLine();
         while (currentLine != null) {
             String[] split;
-            if(currentLine.contains(currentUser)) {
-                System.out.println(currentLine);
+            if (currentLine.contains(currentUser)) {
                 split = currentLine.split(",");
                 termsMap.put(split[0].trim(), split[1].trim());
-                System.out.println(termsMap.entrySet().toString());
-            } if(!(currentLine.contains(currentUser)) && currentLine == null) {
-                termsMap.put(invalidfFirstName, invalidLastName);
-                return termsMap;
             }
             currentLine = bufferedReader.readLine();
         }
         return termsMap;
     }
+
     public static String getCurrentUser() {
         return currentUser;
     }
+
     public static void setCurrentUser(String currentTermList) {
         currentUser = currentTermList;
     }
